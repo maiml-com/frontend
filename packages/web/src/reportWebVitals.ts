9245fc75cd9cb1ecd0914519ec6f13becf9cd76b
Josh G <jg@Joshs-MacBook-Pro.local>
2023-05-29 11:42:25 -0400

    add extra stuff

--- packages/web/src/reportWebVitals.ts
+++ packages/web/src/reportWebVitals.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 const reportWebVitals = (onPerfEntry?: any) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
